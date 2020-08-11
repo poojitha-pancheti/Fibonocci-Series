348bce331fc8a6c52600ad9b0578546e4a01a67c
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 17:27:03 +0530

    Add project files.

---
+++ Fibonocci/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fibonocci
+{
+    class Program
+    {
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+            return Fib(n - 1) + Fib(n - 2);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of terms:");
+            int terms = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i <= terms; i++)
+                Console.Write(Fib(i) + " ");
+        }
+    }
+}
