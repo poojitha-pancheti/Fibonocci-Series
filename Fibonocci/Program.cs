b825e7dc4dea00ac7b04a5604331a79f77207aad
Poojitha Pancheti <Poojitha.Pancheti@DESKTOP-UK1T43N>
2020-08-11 17:35:04 +0530

    First

--- Fibonocci/Program.cs
+++ Fibonocci/Program.cs
@@ -11,7 +11,6 @@ namespace Fibonocci
             if (n == 1)
                 return 1;
             return Fib(n - 1) + Fib(n - 2);
-
         }
         static void Main(string[] args)
         {
